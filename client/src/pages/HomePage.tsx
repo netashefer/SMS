609eb38b25bf856db37219353479d1b8e6a0339f
n <neta111.shefer@gmail.com>
2023-07-02 14:45:07 +0300

    the basics (for nitzan to start)

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import SearchBar from "../components/SearchBar/SearchBar";
+
+const HomePage = () => {
+    return (
+      <div className="HomePage">
+        <SearchBar/>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
